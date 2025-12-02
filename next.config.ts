eab3058dfc2b1ad1a2d92cb165f51539c4c6e1f5
Alberte-Remmer <alre0003@stud.kea.dk>
2025-12-02 11:07:41 +0100

    initialize

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  /* Hvis intet virker prøv at ændre filformat til simpelshop */
+};
+
+export default nextConfig;
