2271e893ff8b6da3d2a4cdc01613a93a08c3746b
Nannasarah <nannasp01@gmail.com>
2025-12-15 13:44:45 +0100

    framermotiononh4

--- app/components/WelcomeCard.jsx
+++ app/components/WelcomeCard.jsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const WelcomeCard = ({ title, icon, pic }) => {
   return (
@@ -10,20 +12,26 @@ const WelcomeCard = ({ title, icon, pic }) => {
           {/* Overlay */}
           <div className="absolute inset-0 bg-black opacity-0 hover:opacity-100 flex items-center justify-center transition-opacity duration-300 border-2 border-y-[#FF2A70] border-x-0">
             <div className="absolute top-0 left-0 w-0 h-0 border-t-50 border-t-[#FF2A70] border-r-50 border-r-transparent"></div>
-            <div
-              className="absolute bottom-0 right-0 w-0 h-0 border-b-50 border-b-[#FF2A70]
-      border-l-50 border-l-transparent"
-            ></div>
+            <div className="absolute bottom-0 right-0 w-0 h-0 border-b-50 border-b-[#FF2A70] border-l-50 border-l-transparent"></div>
 
             <div className="flex flex-col items-center gap-4">
               <div className="border-2 border-[#FF2A70] rounded-[7px] flex items-center justify-center w-max">
                 {icon}
               </div>
-              <h4 className="text-secondary!">{title}</h4>
+
+              <motion.div
+                className="text-secondary!"
+                initial={{ opacity: 0, y: 10 }}
+                whileHover={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                {title}
+              </motion.div>
+
               <p className="text-sm! px-10 text-center">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Doloribus voluptate totam perferendis commodi consectetur quidem
-                similique aspernatur earum eligendi cumque.totam perferendis
+                similique aspernatur earum eligendi cumque. Totam perferendis
                 commodi consectetur quidem similique.
               </p>
             </div>
