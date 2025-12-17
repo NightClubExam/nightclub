f209c3b34aeed5456f3a4bbce5190564c8f00f68
fridacetti <frjo0003@stud.kea.dk>
2025-12-17 09:36:47 +0100

    Suspense sat på

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Testimonials from "./components/Testimonials";
 import RecentBlog from "./components/RecentBlog";
 import Subscription from "./components/Subscription";
 import Footer from "./components/Footer";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -17,11 +18,15 @@ export default function Home() {
       <div className="flex flex-col gap-16">
         <Navigation />
         <Welcome />
+        <Suspense fallback={<div>Loader events...</div>}>
         <Events />
+        </Suspense>
         <Gallery />
         <MediaPlayer />
         <Video />
+        <Suspense fallback={<div>Loader testimonials</div>}>
         <Testimonials />
+        </Suspense>
         <RecentBlog />
         <Subscription />
         <Footer />
